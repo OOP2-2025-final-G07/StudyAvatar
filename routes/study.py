83adc5e7a38eb81628ab10fdc17817f470b25f82
to-sasaki333 <107777777+to-sasaki333@users.noreply.github.com>
2026-01-06 15:07:39 +0900

    タイマーを追加

--- routes/study.py
+++ routes/study.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, abort, current_app
 from datetime import date, datetime
 from models import Study
 
@@ -12,17 +12,32 @@ study_bp = Blueprint('study', __name__, url_prefix='/study')
 def new_study():
     if request.method == 'POST':
 
-        # 日付が入力されていればその日付。されていなければ今日の日付が入ります
-        date_str = request.form.get('date')
-        if date_str:
-            study_date = datetime.strptime(date_str, "%Y-%m-%d").date()
-        else:
-            study_date = date.today()
+        # デバッグ用データがあればタイマーを開始していなくてもデータを記録する
+        debug_date = request.form.get("debug_date")
+        debug_minutes = request.form.get("debug_minutes")
+        if (
+            request.form.get("started") != "1"
+            and not (current_app.debug and (debug_minutes and debug_date))
+        ):
+            abort(400, "タイマーを開始してください")
 
+        # 日付の決定（デバッグデータがあればデバッグデータを優先）
+        if current_app.debug and debug_date:
+            study_date = datetime.strptime(debug_date, "%Y-%m-%d").date()
+        else:
+            date_str = request.form.get("start_date")
+            study_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+
+        # 時間の決定（デバッグデータがあればデバッグデータを優先）
+        if current_app.debug and debug_minutes:
+            minutes = int(debug_minutes)
+        else:
+            minutes = int(request.form["minutes"])
+            
         # データの登録
         Study.create(
             title=request.form['title'],
-            minutes=int(request.form['minutes']),
+            minutes=minutes,
             note=request.form['note'],
             subject=request.form['subject'],
             date=study_date
@@ -30,4 +45,4 @@ def new_study():
         return redirect(url_for('study.new_study'))
 
     studys = Study.select()
-    return render_template('study.html', items=studys)
+    return render_template('study.html', items=studys, debug=current_app.debug)
