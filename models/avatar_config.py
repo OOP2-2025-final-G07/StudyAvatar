5b2df14b93734004633e2bfc7c4f4a45665a4665
kaname <osoranokumokana6@gmail.com>
2026-01-15 13:43:41 +0900

    画像追加
    ・画像追加
    ・難易度選択時に自動でアバター更新

--- models/avatar_config.py
+++ models/avatar_config.py
@@ -1,10 +1,10 @@
 AVATAR_SKINS = {
-    1: "/static/images/1.jpg",
-    2: "/static/images/2.jpg",
-    3: "/static/images/3.jpg",
-    4: "/static/images/4.jpg",
-    5: "/static/images/5.jpg",
-    6: "/static/images/6.jpg",
-    7: "/static/images/7.jpg",
-    8: "/static/images/8.jpg"
+    1: "/static/images/1.PNG",
+    2: "/static/images/2.PNG",
+    3: "/static/images/3.PNG",
+    4: "/static/images/4.PNG",
+    5: "/static/images/5.PNG",
+    6: "/static/images/6.PNG",
+    7: "/static/images/7.PNG",
+    8: "/static/images/8.PNG"
 }
